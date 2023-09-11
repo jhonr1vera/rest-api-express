28c898054f3a6f9c6953191a20c6b953a05bada3
Jhon Rivera <jhonariverab@gmail.com>
2023-09-10 23:15:30 -0400

    delete files

--- scr/routes/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-
-// create endpoint
-router.get('/create', (req, res) => {
-    res.json('jajaja');
-});
-
-module.exports = router; // Enables routing of routes
