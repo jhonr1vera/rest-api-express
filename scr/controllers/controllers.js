54e66bee77679b6246fa9643336dbbc6c123c4f8
Jhon Rivera <jhonariverab@gmail.com>
2023-10-20 15:09:28 -0400

    res.json localized

--- scr/controllers/controllers.js
+++ scr/controllers/controllers.js
@@ -3,13 +3,13 @@ const {connector} = require('../../config/mysql_db');
 const  multer = require('multer')
 const storage = require('../../config/multer');
 const uploader = multer({storage});
+const {i18n} = require('../plugins/i18n-plugin'); 
 
 controller.list = (req, res) => {
         let sql = 'SELECT * FROM new_product';
         connector.query(sql, (err, results) => {
             if(err) throw err;
             res.render('products.ejs', { results })
-            // console.log(results)
         });
 };
 
@@ -20,13 +20,14 @@ controller.save = (req, res) => {
     const domain = req.protocol + '://' + req.headers.host;
     let url = `${domain}/images/${file.filename}`;
     const sql = `INSERT INTO new_product (product_name, product_price, product_detail, product_img) VALUES (?, ?, ?, ?)`;
-
-      connector.query(sql, [body.productName, body.productPrice, body.productDetail, url], (err, results) => {
-          if(err) {
-              res.status(500).json({ message: 'There was an error inserting data' });
+    
+    i18n.init(req, res)  
+    connector.query(sql, [body.productName, body.productPrice, body.productDetail, url], (err, results) => {
+        if(err) {
+              res.status(500).json({ message: req.__('There was an error inserting data') });
           } else {
               res.status(200).json({
-                  message: 'Data added correctly',
+                  message: req.__('Data added correctly'),
                   product: {
                       productName: body.productName,
                       productPrice: body.productPrice,
@@ -44,11 +45,11 @@ controller.save = (req, res) => {
 controller.edit = (req, res) => {
     const {product_id} = req.params;
     const query = `SELECT * FROM new_product WHERE product_id = ?`;
-
+    i18n.init(req, res)  
     connector.query(query, [product_id], (error, results) => {
         if (error) {
             console.log(error);
-            res.status(500).json('An error occurred');
+            res.status(500).json(req.__('An error occurred'));
         } else {
             res.render('edit.ejs', {data : results[0]});
         }
@@ -56,22 +57,21 @@ controller.edit = (req, res) => {
 };
 
 controller.update = (req, res) => {
-
     const {product_id} = req.params;
     const data = req.body;
     const file = req.file;
     const domain = req.protocol + '://' + req.headers.host;
     let url = `${domain}/images/${file.filename}`;
-
     const sql = `UPDATE new_product SET product_name = ?, product_price = ?, product_detail = ?, product_img = ? WHERE product_id = ?`;
 
+    i18n.init(req, res)  
     connector.query(sql, [data.productName, data.productPrice, data.productDetail, url, product_id ], (error, results) => {
         if (error) {
           console.log(error);
-          res.status(500).json({ message: 'There was an error updating data' });
+          res.status(500).json({ message: req.__('There was an error updating data') });
         } else {
           res.status(200).json({
-            message: 'Data updated correctly',
+            message: req.__('Data updated correctly'),
             product: {
               productName: data.productName,
               productPrice: data.productPrice,
@@ -85,28 +85,29 @@ controller.update = (req, res) => {
 
 controller.delete = (req, res) => {
     const {product_id} = req.params;
-    // const query = ;
 
+    i18n.init(req, res)  
     connector.query(`DELETE FROM new_product WHERE product_id = ?`, [product_id], (error, results) => {
         if (error) {
             console.log(error);
-            res.status(500).json('An error occurred');
+            res.status(500).json(req.__('An error occurred'));
         } else {
-            res.status(200).json('data deleted');
+            res.status(200).json(req.__('Data deleted'));
         }
     });
 };
 
 controller.select = (req, res) => {
-  const {product_id} = req.params;
-  const query = `SELECT *, product_img FROM new_product WHERE product_id = ?`;
+    const {product_id} = req.params;
+    const query = `SELECT *, product_img FROM new_product WHERE product_id = ?`;
 
-  connector.query(query, [product_id], (error, results) => {
-      if (error) {
-          console.log(error);
-          res.status(500).json('An error occurred');
-      } else {
-        res.render('article.ejs', { data : results[0] });
+    i18n.init(req, res)  
+    connector.query(query, [product_id], (error, results) => {
+        if (error) {
+            console.log(error);
+            res.status(500).json(req.__('An error occurred'));
+        } else {
+            res.render('article.ejs', { data : results[0] });
       }
   });
 };
